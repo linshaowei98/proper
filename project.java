71d0c2fcbe8ffc2ea90a232465a69886a931cfae
ljk <731236067@qq.com>
2019-06-15 17:24:28 +0800

    添加test工程到Git

---
+++ project.java
@@ -0,0 +1,9 @@
+package com.itheima.test1;
+
+public class project {
+    public static void main(String[] args) {
+        System.out.println("skr!");
+        System.out.println("skr!");
+        System.out.println("skr!");
+    }
+}
